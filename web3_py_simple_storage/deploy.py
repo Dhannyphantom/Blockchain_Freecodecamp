0825ca9fd77978f9510decb9c95725edcc78401c
Dhannyphantom <64708060+Dhannyphantom@users.noreply.github.com>
2022-05-24 00:32:12 +0100

    Refactored the project structure

--- web3_py_simple_storage/deploy.py
+++ web3_py_simple_storage/deploy.py
@@ -1,5 +1,6 @@
 from solcx import compile_standard, install_solc
 import json
+from web3 import Web3
 
 my_solc_verion = "0.6.0"
 
@@ -30,3 +31,8 @@ with open("./compiled_code.json", "w") as file:
 
 bytecode = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["evm"]["bytecode"]["object"]
 abi = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["abi"]
+
+# TO CONNECT TO GANACHE
+w3 = Web3(Web3.HTTPPROVIDER("http://127.0.0.1:7545"))
+
+print(w3.isConnected())

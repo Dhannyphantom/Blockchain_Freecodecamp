0ed80d26d5cb621bfc421a08c7c7f1bae5d00468
Dhannyphantom <64708060+Dhannyphantom@users.noreply.github.com>
2022-05-27 13:31:04 +0100

    FundMe is able to get AggregatorV3 Version

--- test_fundme/contracts/FundMe.sol
+++ test_fundme/contracts/FundMe.sol
@@ -2,18 +2,29 @@
 
 pragma solidity >=0.6.0 <0.9.0;
 
-import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+// import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+// import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "../node_modules/@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 contract FundMe {
     
     mapping (address=>uint) public addressToAmount;
 
     function fund() public payable {
-        addressToAmount[msg.sender] += msg.value
+        addressToAmount[msg.sender] += msg.value;
     }
 
+    // RINKEBY PRICE FEED ADDRESS IS USED
+
     function getVersion() public view returns (uint) {
-        AggregatorV3Interface priceFeed = AggregatorV3Interface()
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
+        return priceFeed.version();
+    }
+
+    function getPrice() public view returns (uint) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
+        (, int price,,,) = priceFeed.latestRoundData();
+        return uint(price);
     }
 
 }
--- test_fundme/scripts/deploy.py
+++ test_fundme/scripts/deploy.py
@@ -1,12 +1,18 @@
-from brownie import accounts, FundMe
+from brownie import accounts,network, FundMe, config
 
 def deploy_test_fundme():
-    account = accounts[0]
+    account = getAccount()
 
     fund_me = FundMe.deploy({"from": account})
     
-    fund_txn = fund_me.fund(value=value)
+    fund_txn = fund_me.getVersion()
+    print(fund_txn)
 
+def getAccount():
+    if network.show_active() == "development":
+        return accounts[0]
+    else:
+        return accounts.add(config["wallets"]["from_key"])
 
 def main():
     deploy_test_fundme()

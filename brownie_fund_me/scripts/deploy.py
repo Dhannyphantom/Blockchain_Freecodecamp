c463db6c728dc71a288faa22b917882fa63a9c99
Dhannyphantom <64708060+Dhannyphantom@users.noreply.github.com>
2022-05-27 19:34:14 +0100

    Auto-verification of rinkeby network using API keys

--- brownie_fund_me/scripts/deploy.py
+++ brownie_fund_me/scripts/deploy.py
@@ -3,7 +3,7 @@ from scripts.reusable_scripts import get_account
 
 def deploy_fund_me():
     account = get_account()
-    fund_me = FundMe.deploy({"from": account})
+    fund_me = FundMe.deploy({"from": account}, publish_source=True)
     print(f"Contract deployed to {fund_me.address} Yay!!!!!!")
 
 def main():
